5d9c4185239c3cfd0e5ed172ff55832b244511a2
sungmin69355 <sungmin69355@naver.com>
2023-01-21 14:37:51 +0900

    :fire: SpringBootApplication를 사용 x -> 직접 서블릿 컨테이너 띄우기(동작원리 이해)

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,12 +2,16 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //서블릿 웹서버를 쉽게만들어주는 팩토리
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
