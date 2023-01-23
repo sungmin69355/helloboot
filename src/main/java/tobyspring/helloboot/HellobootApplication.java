ea3c6718c1353b4096177a3f53ab7d17e041455e
sungmin69355 <sungmin69355@naver.com>
2023-01-23 21:02:34 +0900

    :fire: 스프링 컨테이너로 통합작업

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -26,20 +26,26 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 
 		//직접 스프링컨테이너 생성
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //서블릿 웹서버를 쉽게만들어주는 팩토리
+				//익명 함수
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							//DispatcherServlet은 웹환경에서 좀더 편하게 사용할 수 있는 하나의 서블릿(WebApplicationContext를 사용해야한다.)
+							new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+
+			}
+		};
 		applicationContext.registerBean(HelloController.class); // bean 등록의 순서는 스프링이 알아서 적절하게 등록해준다.
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //서블릿 웹서버를 쉽게만들어주는 팩토리
-		//익명 함수
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					//DispatcherServlet은 웹환경에서 좀더 편하게 사용할 수 있는 하나의 서블릿(WebApplicationContext를 사용해야한다.)
-					new DispatcherServlet(applicationContext)
-					).addMapping("/*");
-		});
-		webServer.start();
 	}
 
 }
