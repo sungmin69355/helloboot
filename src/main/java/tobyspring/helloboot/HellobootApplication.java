cd632a954811f21a29810aa2ef5d122a8f18518d
sungmin69355 <sungmin69355@naver.com>
2023-01-24 19:16:35 +0900

    :fire: Configuration 작성해서 자바코드로 구성

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -21,12 +24,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
 
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 	public static void main(String[] args) {
 
 		//직접 스프링컨테이너 생성
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -43,8 +55,7 @@ public class HellobootApplication {
 
 			}
 		};
-		applicationContext.registerBean(HelloController.class); // bean 등록의 순서는 스프링이 알아서 적절하게 등록해준다.
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
 	}
 
