0a625467ab0a3d282a421e70423689f6087e0488
sungmin69355 <sungmin69355@naver.com>
2023-01-25 21:32:00 +0900

    :fire: 팩토리 메서드로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -13,6 +14,16 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
 	public static void main(String[] args) {
 
 		//직접 스프링컨테이너 생성
@@ -21,13 +32,18 @@ public class HellobootApplication {
 			protected void onRefresh() {
 				super.onRefresh();
 
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //서블릿 웹서버를 쉽게만들어주는 팩토리
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+				// 아래 코드를 삭제해도 dispatcherServlet이 잘작동된다.
+				// 그 이유는 스프링컨테이너가 dispatcherServlet는 ApplicationContext가 필요하다는 것을 인지하고 주입한다.
+				// 동작원리는 dispatcherServlet가 구현하고있는 인터페이스 중 ApplicationContextAware 인터페이스에서 setApplicationContext 메서드를 통해서 넣어준다. (빈생성시점)
+				dispatcherServlet.setApplicationContext(this);
+
 				//익명 함수
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							//DispatcherServlet은 웹환경에서 좀더 편하게 사용할 수 있는 하나의 서블릿(WebApplicationContext를 사용해야한다.)
-							new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*");
 				});
 				webServer.start();
 
