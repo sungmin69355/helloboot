9a0b846ece064137ed5ffa799bb8d794e0720040
sungmin69355 <sungmin69355@naver.com>
2023-03-19 20:53:10 +0900

    :white_check_mark: jdbcTemplate Test

---
+++ src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,28 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloBootTest
+public class JdbcTemplateTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int )");
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "sungmin", 1);
+        jdbcTemplate.update("insert into hello values(?, ?)", "spring", 3);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+}
